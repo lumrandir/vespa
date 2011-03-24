5ef404b9ccd730454f04b138d4e5841ccfeab56e
Lumren Randir <lumrandir@gmail.com>
2011-03-24 14:56:29 +0500

    ran out of monad

--- Distributions.hs
+++ Distributions.hs
@@ -1,38 +1,24 @@
 module Distributions where
 
 import Data.List
-import System.Random
-import Control.Monad
 
-{-
-getY :: (Floating t) => t -> t -> t -> t
+getY :: Double-> Double-> Double -> Double
 getY m d x = 1.0 / (sqrt (2 * pi * d)) * exp (- (x - m)^2 / (2 * d))
 
-getDistrLine :: (Floating t) => [t] -> [t]
+getDistrLine :: [Double] -> [Double]
 getDistrLine xs = map (* k) xs
 	     	  where k = 1 / (head xs)
 
-randomSequence :: (Floating t, Random t, Integral t1) => (t, t) -> t1 -> [t] -> [t]
-randomSequence range 0 xs    = xs
-randomSequence range seed xs = randomSequence range (seed - 1) (x:xs)
-		     	       where x = fst $ (randomR range newGen)
-			       		 where newGen = snd $ randomR range (mkStdGen seed)
 -- returns an order value, takes one three-tuple, which is a tuple of parameters:
 -- mathematical waiting, dispersion and edges (like (1,19))
-getOrder :: (Floating t) => (t, t, (t, t ), Int) -> Maybe Int
-getOrder (m, d, (l, r), seed) = findIndex (\x -> x >= y) (tail (scanl (+) 0 line))
-	   		  	where line = map round (getDistrLine (map (getY m d) [l..r]))
-				      y    = randomSequence (l, upperBound) seed [] !! seed
-		     		             where upperBound = sum line
--}
+getNormal :: (Double, Double, (Double, Double)) -> Double -> Maybe Int
+getNormal (m, d, (l, r)) rnd = findIndex (\x -> x >= rnd) (tail (scanl (+) 0 line))
+  where line = getDistrLine $ map (getY m d) [l..r]
 
-geomDecide :: Int -> Int
-geomDecide day =
-  | liftM2 (<) rand (return (getGeomDistr (1, 20) !! day) :: IO Double) = 0
-  | otherwise = 1
-  
-getGeomDistr :: (Double, Double) -> [Double]
-getGeomDistr = undefined
+getNormalUpperBound (m, d, (l, r)) = sum list
+  where list = tail $ scanl (+) 0 line
+        line = getDistrLine $ map (getY m d) [l..r]
 
-rand :: IO Double
-rand = undefined
+getGeom :: (Double, Double) -> [Double]
+getGeom = undefined
+
--- Main.hs
+++ Main.hs
@@ -1,13 +1,26 @@
 module Main where
 
 import Upkeep
+import Distributions
 import Graphics.Rendering.Chart.Simple
 import Data.List
+import System.Random
+import Control.Monad
 
 main :: IO ()
 main = do
-  let fixedPeriodUpkeep = [ getUpkeep x 10 | x <- [1..20] ]
-  let fixedSizeUpkeep   = [ getUpkeep 10 x | x <- [1..20] ]
+-- сначала достанем верхнее значение нашего нормального распределения
+  let upperNormal = getNormalUpperBound (5, 10, (1, 19))
+-- теперь сгенерим бесконечный список случайных значений в пределах от 1 до верхнего значения
+  let listNormal = getStdGen >>= (\g -> return (randomRs (1, upperNormal) g))
+-- теперь проделаем ту же самую фигню для геометрического распределения
+  let upperGeom = getGeomUpperBound
+  let listGeom = getStdGen >>= (\g -> return (randomRs (1, upperGeom) g))
+-- пакуем их в кортеж
+  let rnd = liftM2 (\x y -> (x, y)) listNormal listGeom
+-- теперь запустим вычисления, вбросив этот кортеж в качестве источников рандомных чисел
+--  let fixedPeriodUpkeep = [ getUpkeep x 10 | x <- [1..20] ]
+--  let fixedSizeUpkeep   = [ getUpkeep 10 x | x <- [1..20] ]
 --  putStrLn "ssss"
 --  plotPDF "fixedPeriod.pdf" ([1..20] :: [Double]) (fixedPeriodUpkeep :: [Double]) "Upkeep with constant order period"
 --  plotPDF "fixedSize.pdf" ([1..20] :: [Double]) (fixedSizeUpkeep :: [Double]) "Upkeep with constant order size"
