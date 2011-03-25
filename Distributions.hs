91d453d23be6f136c8b4271d9fdc395abd79298f
Lumren Randir <lumrandir@gmail.com>
2011-03-25 14:10:26 +0500

    added geometrical distribution

--- Distributions.hs
+++ Distributions.hs
@@ -2,23 +2,28 @@ module Distributions where
 
 import Data.List
 
-getY :: Double-> Double-> Double -> Double
-getY m d x = 1.0 / (sqrt (2 * pi * d)) * exp (- (x - m)^2 / (2 * d))
+getNormalY :: Double-> Double-> Double -> Double
+getNormalY m d x = 1.0 / (sqrt (2 * pi * d)) * exp (- (x - m)^2 / (2 * d))
 
 getDistrLine :: [Double] -> [Double]
 getDistrLine xs = map (* k) xs
 	     	  where k = 1 / (head xs)
 
--- returns an order value, takes one three-tuple, which is a tuple of parameters:
--- mathematical waiting, dispersion and edges (like (1,19))
 getNormal :: (Double, Double, (Double, Double)) -> Double -> Maybe Int
 getNormal (m, d, (l, r)) rnd = findIndex (\x -> x >= rnd) (tail (scanl (+) 0 line))
-  where line = getDistrLine $ map (getY m d) [l..r]
+  where line = getDistrLine $ map (getNormalY m d) [l..r]
 
+getNormalUpperBound :: (Double, Double, (Double, Double)) -> Double
 getNormalUpperBound (m, d, (l, r)) = sum list
   where list = tail $ scanl (+) 0 line
-        line = getDistrLine $ map (getY m d) [l..r]
+        line = getDistrLine $ map (getNormalY m d) [l..r]
 
-getGeom :: (Double, Double) -> [Double]
-getGeom = undefined
+getGeom :: Double -> (Double, Double) -> [Double]
+getGeom initialP (l, r) = map (getGeomY initialP) [l..r]
+
+getGeomY :: Double -> Double -> Double
+getGeomY p n = 1 - ((1 - p) ** n)
+
+getGeomUpperBound :: Double -> (Double, Double) -> Double
+getGeomUpperBound initialP (l, r) = head $ reverse $ map (getGeomY initialP) [l..r]
 
