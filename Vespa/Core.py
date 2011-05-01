3edf54db140e761079a0b9cdc9e02382ab0b6b94
lumrandir <lumrandir@lumren-ubuntu.(none)>
2011-05-02 00:18:31 +0600

    integration with haskell core

--- Vespa/Core.py
+++ Vespa/Core.py
@@ -1,10 +1,40 @@
 from subprocess import call
+from PyQt4.QtCore import QMimeData
 
-def callTheDemon(vessel):
-        invocation = "./vespa \"{args}\"".format(args=collectTheBlood(vessel))
+def calculateTheApocalypse(vessel):
+        blood = collectTheBlood(vessel)
+        if not callTheDemon(blood) == 0:
+                print("Noe gikk galt...")
+                return
+        return 
+
+def callTheDemon(blood):
+        invocation = "./vespa \"{args}\"".format(args=blood)
         exitCode = call(invocation, shell=True)
         return exitCode
 
 def collectTheBlood(vessel):
-        return ()
+        vial = (int(vessel.initialStorageEdit.text()),
+                        int(vessel.countPeriodEdit.text()),
+                        int(vessel.fixedPeriodEdit.text()),
+                        int(vessel.fixedSizeEdit.text()),
+                        int(vessel.maxPeriodEdit.text()),
+                        int(vessel.maxSizeEdit.text()),
+                        int(vessel.unitCostEdit.text()),
+                        int(vessel.orderCostEdit.text()),
+                        int(vessel.deficitCostEdit.text())
+               )
+        buf = []
+        for row in range(0, vessel.ordTable.rowCount()):
+                buf = buf + [(int(vessel.ordTable.item(row, 0).text()), 
+                                float(vessel.ordTable.item(row, 1).text())
+                            )]
+        vial = vial + (buf,)
+        buf = []
+        for row in range(0, vessel.delTable.rowCount()):
+                buf = buf + [(int(vessel.delTable.item(row, 0).text()),
+                                float(vessel.delTable.item(row, 1).text())
+                            )]
+        vial = vial + (buf,)
+        return vial
 
--- Vespa/Forms/MainForm.py
+++ Vespa/Forms/MainForm.py
@@ -1,17 +1,44 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Form.ui'
-#
-# Created: Sun May  1 10:17:25 2011
-#      by: PyQt4 UI code generator 4.7.4
-#
-# WARNING! All changes made in this file will be lost!
-
 from Vespa.Forms.uiForm import Ui_Form
-from PyQt4.QtGui import QWidget
+from PyQt4.QtGui import QWidget, QTableWidgetItem
+from functools import partial
+from PyQt4 import QtCore
+from Vespa import Core
 
 class Form(QWidget, Ui_Form):
         def __init__(self, parent=None):
                 super(Form, self).__init__(parent)
                 self.setupUi(self)
+
+                calcCallback = partial(Core.calculateTheApocalypse, self)
+                self.connect(self.addOrdButton, QtCore.SIGNAL("clicked()"), self.addOrdRow)
+                self.connect(self.removeOrdButton, QtCore.SIGNAL("clicked()"), self.removeOrdRow)
+                self.connect(self.clearOrdButton, QtCore.SIGNAL("clicked()"), self.clearOrdTable)
+                self.connect(self.addDelButton, QtCore.SIGNAL("clicked()"), self.addDelRow)
+                self.connect(self.removeDelButton, QtCore.SIGNAL("clicked()"), self.removeDelRow)
+                self.connect(self.clearDelButton, QtCore.SIGNAL("clicked()"), self.clearDelTable)
+                self.connect(self.calcButton, QtCore.SIGNAL("clicked()"), calcCallback)
+
+        def addOrdRow(self):
+                self.ordTable.insertRow(0)
+                self.ordTable.setItem(0, 0, QTableWidgetItem(self.ordSizeEdit.text()))
+                self.ordTable.setItem(0, 1, QTableWidgetItem(self.ordProbEdit.text()))
+
+        def removeOrdRow(self):
+                self.ordTable.removeRow(self.ordTable.currentRow())
+
+        def clearOrdTable(self):
+                self.ordTable.setRowCount(0)
+
+        def addDelRow(self):
+                self.delTable.insertRow(0)
+                self.delTable.setItem(0, 0, QTableWidgetItem(self.delSizeEdit.text()))
+                self.delTable.setItem(0, 1, QTableWidgetItem(self.delProbEdit.text()))
+
+        def removeDelRow(self):
+                self.delTable.removeRow(self.delTable.currentRow())
+
+        def clearDelTable(self):
+                self.delTable.setRowCount(0)
 
